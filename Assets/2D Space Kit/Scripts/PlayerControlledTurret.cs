a25ec96d2c059de60e7023c1e4c90614dd12405b
Lucas Idiart <lucas.idiart@jala.university>
2025-02-11 19:48:54 -0300

    remove aim

--- Assets/2D Space Kit/Scripts/PlayerControlledTurret.cs
+++ Assets/2D Space Kit/Scripts/PlayerControlledTurret.cs
@@ -18,8 +18,7 @@ public class PlayerControlledTurret : MonoBehaviour {
 	
 		//This makes the turret aim at the mouse position (Controlled by CustomPointer, but you can replace CustomPointer.pointerPosition with Input.MousePosition and it should work)
 		Vector2 turretPosition = Camera.main.WorldToScreenPoint(transform.position);
-		Vector3 direction = CustomPointer.pointerPosition - turretPosition;
-		transform.rotation = Quaternion.Euler (new Vector3(0, 0, Mathf.LerpAngle(transform.rotation.eulerAngles.z, (Mathf.Atan2 (direction.y,direction.x) * Mathf.Rad2Deg) - 90f, turret_rotation_speed * Time.deltaTime)));
+		transform.rotation = Quaternion.Euler (new Vector3(0, 0, Mathf.LerpAngle(transform.rotation.eulerAngles.z, (Mathf.Atan2 (0,0) * Mathf.Rad2Deg) - 90f, turret_rotation_speed * Time.deltaTime)));
 
 
 		if (Input.GetMouseButtonDown(0) && barrel_hardpoints != null) {
